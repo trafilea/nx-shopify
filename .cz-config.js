1b07e841d1604eef2f57f7fc29728778cae65938
Sebastiandg7 <sebastiandg7@gmail.com>
2020-10-17 14:15:47 -0500

    feat(repo): add commitizen

---
+++ .cz-config.js
@@ -0,0 +1,73 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat:     A new feature' },
+    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'docs', name: 'docs:     Documentation only changes' },
+    {
+      value: 'cleanup',
+      name:
+        'cleanup:  A code change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'chore',
+      name: "chore:    Other changes that don't modify src or test files",
+    },
+  ],
+
+  scopes: [
+    { name: 'build', description: "anything 'build' builder specific" },
+    { name: 'deploy', description: "anything 'deploy' builder specific" },
+    { name: 'serve', description: "anything 'serve' builder specific" },
+    { name: 'theme', description: "anything 'theme' schematic specific" },
+    { name: 'core', description: 'anything workspace specific' },
+    { name: 'docs', description: 'anything related to docs infrastructure' },
+    {
+      name: 'repo',
+      description: 'anything related to managing the repo itself',
+    },
+    { name: 'misc', description: 'misc stuff' },
+  ],
+
+  allowTicketNumber: true,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*
+    scopeOverrides: {
+      fix: [
+        {name: 'merge'},
+        {name: 'style'},
+        {name: 'e2eTest'},
+        {name: 'unitTest'}
+      ]
+    },
+    */
+  // override the messages, defaults are as follows
+  messages: {
+    type: "Select the type of change that you're committing:",
+    scope: '\nDenote the SCOPE of this change (optional):',
+    // used if allowCustomScopes is true
+    customScope: 'Denote the SCOPE of this change:',
+    subject:
+      'Write a SHORT, IMPERATIVE (lowercase) description of the change:\n',
+    body:
+      'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer:
+      'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?',
+  },
+
+  allowCustomScopes: false,
+  allowBreakingChanges: ['feat', 'fix'],
+  // skip any questions you want
+  skipQuestions: ['ticketNumber'],
+
+  // limit subject length
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
+};
