f7e63e885e7562b6714c00f1417652c4df37a25f
Sebastiandg7 <sebastiandg7@gmail.com>
2020-10-20 13:32:50 -0500

    cleanup(e2e): make use of a single nx workspace for e2e tests

--- e2e/nx-shopify-e2e/tests/nx-shopify.test.ts
+++ e2e/nx-shopify-e2e/tests/nx-shopify.test.ts
@@ -6,11 +6,13 @@ import {
   uniq,
 } from '@nrwl/nx-plugin/testing';
 describe('nx-shopify e2e', () => {
+  beforeAll(() => {
+    ensureNxProject('@trafilea/nx-shopify', 'dist/packages/nx-shopify');
+  });
+
   it('should create nx-shopify', async (done) => {
     const plugin = uniq('nx-shopify');
-    ensureNxProject('@trafilea/nx-shopify', 'dist/packages/nx-shopify');
     await runNxCommandAsync(`generate @trafilea/nx-shopify:theme ${plugin}`);
-
     const result = await runNxCommandAsync(`build ${plugin}`);
     expect(result.stdout).toContain('Successfully built');
 
@@ -20,7 +22,6 @@ describe('nx-shopify e2e', () => {
   describe('--directory', () => {
     it('should create src in the specified directory', async (done) => {
       const plugin = uniq('nx-shopify');
-      ensureNxProject('@trafilea/nx-shopify', 'dist/packages/nx-shopify');
       await runNxCommandAsync(
         `generate @trafilea/nx-shopify:theme ${plugin} --directory subdir`
       );
@@ -34,7 +35,6 @@ describe('nx-shopify e2e', () => {
   describe('--tags', () => {
     it('should add tags to nx.json', async (done) => {
       const plugin = uniq('nx-shopify');
-      ensureNxProject('@trafilea/nx-shopify', 'dist/packages/nx-shopify');
       await runNxCommandAsync(
         `generate @trafilea/nx-shopify:theme ${plugin} --tags e2etag,e2ePackage`
       );
