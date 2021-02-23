13afd8925d0a3c5e14c56d3bfa4fb0d68cc6b1c7
gh-actions <actions@gihub.com>
2021-02-23 23:25:35 +0000

    Deploy website - based on d9050376fd3b3652dd76e54e22140d609c60e605

---
+++ 60ab3699.0e5c5c21.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{80:function(o){o.exports=JSON.parse('{"allTagsPath":"/nx-shopify/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/nx-shopify/blog/tags/hola"}')}}]);
---
+++ ae7d1de8.a0ae6532.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{86:function(o){o.exports=JSON.parse('{"permalink":"/nx-shopify/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
